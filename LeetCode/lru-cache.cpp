1b7452957d9841bc892d37bb4c0fa28d71ab8820
Kriith <kriithnsethi@gmail.com>
2025-03-15 16:59:08 +0530

    daily commit

--- LeetCode/lru-cache.cpp
+++ LeetCode/lru-cache.cpp
@@ -1,57 +1,64 @@
 class LRUCache {
     public:
-        int cap = 0, size = 0;
-        class node{
-            public : 
+        struct node{
+            node *next;
+            node *prev;
             int key, val;
-            node *next, *prev;
-            node(int x, int y){
-                key = x, val = y;
-                next = prev = nullptr;
+            node(int k, int v){
+                key = k;
+                val = v;
+                next = nullptr;
+                prev = nullptr;
             }
         };
     
-        node *head = new node(-1, -1);
-        node* tail = new node(-1, -1);
-    
-        void add(node *n){
-            node *t = head->next;
-            t->prev = n;
-            head->next = n;
-            n->next = t;
-        }
-    
-        void deletenode(node *n){
-            node *nx = n->next, *pv = n->prev;
-            nx->prev = pv;
-            pv->next = nx;
-        }
-    
+        int cap = 0;
+        node *head, *tail;
+        map<int, node*> mp;
         LRUCache(int capacity) {
             cap = capacity;
+            head = new node(-1, -1);
+            tail = new node(-1, -1);
             head->next = tail;
             tail->prev = head;
         }
         
         int get(int key) {
-            node *t = head;
-            while(t != nullptr && t->key != key) t = t->next;
-            if(t == nullptr) return -1;
-            return t->val;
-            deletenode(t);
-            add(t);
+            if(mp.find(key) == mp.end()) return -1;
+            int v = mp[key]->val;
+            update(mp[key]);
+            return v;
         }
         
         void put(int key, int value) {
-            node *t = head;
-            while(t != nullptr && t->key != key) t = t->next;
-            if(t == nullptr){
-                t = new node(key, value);
-                add(t);
-                size++;
-                if(size > cap){ size--; deletenode(tail->prev); }
+            if(mp.find(key) != mp.end()){
+                node *t = mp[key];
+                t->prev->next = t->next;
+                t->next->prev = t->prev;
+                mp.erase(key);
             }
-            else { deletenode(t); add(t); }
+            if(mp.size() == cap){
+                node *t = head->next;
+                mp.erase(t->key);
+                head->next = t->next;
+                t->next->prev = head;
+            }
+            node *t = new node(key, value);
+            mp[key] = t;
+            head->next->prev = t;
+            t->next = head->next;
+            head->next = t;
+            t->prev = head;
+            update(t);
+        }
+    
+        void update(node *t){
+            t->next->prev = t->prev;
+            t->prev->next = t->next;
+            t->prev = tail->prev;
+            t->next = tail;
+            tail->prev->next = t;
+            tail->prev = t;
         }
     };
     
