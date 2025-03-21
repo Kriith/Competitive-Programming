ff691b605c62e3790cf7b0a7e5f05729b42f33dd
Kriith <kriithnsethi@gmail.com>
2025-03-21 23:24:26 +0530

    commit

---
+++ Codeforces/practice/2067b.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using vll = vector<long long>;
+using vvi = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+using mint = map<int, int>;
+using mll = map<ll, ll>;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define loop(i, a, b) for(int i = a; i<b; i++)
+#define pool(i, a, b) for(int i = a; i>b; i--)
+#define lop(i, n) for(int i = 0; i<n; i++)
+#define lopp(i, n) for(int i = 1; i<n; i++)
+#define eb emplace_back
+#define ef emplace_front
+#define pb push_back
+#define puf push_front
+#define popb pop_back
+#define popf pop_front
+#define ff first
+#define ss second
+#define nl "\n"
+#define sp " "
+#define mie *min_element
+#define mxe *max_element
+#define all(x) x.begin(), x.end()
+
+#ifndef ONLINE_JUDGE
+#include "C:/Users/HP/Desktop/Competitive Programming/debug.h"
+#else
+#define dbg(...) 42
+#endif
+
+void solve(){
+    int n;
+    cin>>n;
+    vi a(n);
+    lop(i, n) cin>>a[i];
+    sort(all(a));
+
+
+    //logic : i is incremented twice, have to make pairs of a[i]
+    //if a[i] = a[i+1] then no issue
+    //if a[i] has 3 occurences, then the last one should be inc by one 
+    //to make a pair
+    //therefore mx is inc by one with each time
+    int mx = 0; 
+    lop(i, n){
+        if(max(mx, a[i]) != max(mx, a[i + 1])){
+            cout<<"No\n";
+            return;
+        }
+        mx = max(a[i++], mx) + 1;
+    }
+    cout<<"Yes\n";
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("C:/Users/HP/Desktop/Competitive Programming/input.txt", "r", stdin);
+    freopen("C:/Users/HP/Desktop/Competitive Programming/output.txt", "w", stdout);
+#endif
+    ll t = 1;
+    cin>>t; 
+    while(t--)
+        solve();
+    return 0;
+}
